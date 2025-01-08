8c946f0360db2af6cd7df65ee9d8eb968c5f7d8a
Sourav-student <so34ur56@gmail.com>
2025-01-08 20:42:05 +0530

    update

--- 01_Infinite.c
+++ 01_Infinite.c
@@ -1,14 +1,14 @@
-// Write a Program to print hello world infinite times
+// Write a C program to print hello World Infinite time
 
 #include <stdio.h>
 
-int main(){
-  
+int main()
+{
   int n;
   while (n = 1)
   {
     printf("Hello World\n");
   }
-  
+
   return 0;
 }
